1eda6f71f0638c40f91cdc1f6a9c45732af56f10
Hendra Kurniawan <IT181123@BTPN>
2019-02-26 21:50:50 +0700

    first_commit

---
+++ src/main/java/com/project/gotter/GotterApplication.java
@@ -0,0 +1,13 @@
+package com.project.gotter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotterApplication.class, args);
+	}
+
+}
